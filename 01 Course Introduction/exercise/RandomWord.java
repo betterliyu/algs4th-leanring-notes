bd84889a25ace0efbcf656c9ce5d847a1c27e227
李雨 <liyu@ujing.com.cn>
2022-06-09 15:52:13 +0800

    fix assignment 01

--- 01 Course Introduction/exercise/RandomWord.java
+++ 01 Course Introduction/exercise/RandomWord.java
@@ -4,8 +4,22 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
     public static void main(String[] args) {
-
+        int index = 0;
+        String champion = null;
+        while (!StdIn.isEmpty()) {
+            index++;
+            String cur = StdIn.readString();
+            double p = 1.0 / index;
+            if (StdRandom.bernoulli(p)) {
+                champion = cur;
+            }
+        }
+        StdOut.println(champion);
     }
 }
